ff85ed8dfa7fce6e9ad479741fb361fe62b1cf49
Sean Frommelt <sean.fromwhere@gmail.com>
2020-06-01 08:50:16 +1000

    moved the board printing function to the SudokuBoard class, now allows user to see different versions of the solved puzzle or terminate the program

--- SudokuBoard.py
+++ SudokuBoard.py
@@ -14,6 +14,7 @@ class SudokuBoard:
 		]
 		#a b c d e f g h i
 
+		#This is some hardcoded spaghett
 		self.quadrants = [
 		[(0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)], #q1
 		[(3,0), (3,1), (3,2), (4,0), (4,1), (4,2), (5,0), (5,1), (5,2)], #q2
@@ -36,8 +37,21 @@ class SudokuBoard:
 							self.solve()
 							self.board[y][x] = 0
 					return
+		self.printBoard()
+		terminate = input("Stop? Y/N ")
+		if terminate == 'y' or terminate == 'Y':
+			exit()
+		elif terminate == 'n' or terminate == 'N':
+			pass
+		else:
+			pass
+
 		
-		
+	def placeValue(self, position, value):
+		x = position[0]
+		y = position[1]
+		self.board[y][x] = value
+		return
 
 	def checkValidPlacement(self, position, value):
 		x = position[0]
@@ -67,3 +81,22 @@ class SudokuBoard:
 
 	def returnValue(self):
 		return self.board
+
+	def printBoard(self):
+		print()
+		print("    a  b  c   d  e  f   g  h  i")
+		print("   _____________________________")
+		print("1 | {}  {}  {} | {}  {}  {} | {}  {}  {} | 1".format(self.board[0][0],self.board[0][1],self.board[0][2],self.board[0][3],self.board[0][4],self.board[0][5],self.board[0][6],self.board[0][7],self.board[0][8]))
+		print("2 | {}  {}  {} | {}  {}  {} | {}  {}  {} | 2".format(self.board[1][0],self.board[1][1],self.board[1][2],self.board[1][3],self.board[1][4],self.board[1][5],self.board[1][6],self.board[1][7],self.board[1][8]))
+		print("3 | {}  {}  {} | {}  {}  {} | {}  {}  {} | 3".format(self.board[2][0],self.board[2][1],self.board[2][2],self.board[2][3],self.board[2][4],self.board[2][5],self.board[2][6],self.board[2][7],self.board[2][8]))
+		print("  |-----------------------------|")
+		print("4 | {}  {}  {} | {}  {}  {} | {}  {}  {} | 4".format(self.board[3][0],self.board[3][1],self.board[3][2],self.board[3][3],self.board[3][4],self.board[3][5],self.board[3][6],self.board[3][7],self.board[3][8]))
+		print("5 | {}  {}  {} | {}  {}  {} | {}  {}  {} | 5".format(self.board[4][0],self.board[4][1],self.board[4][2],self.board[4][3],self.board[4][4],self.board[4][5],self.board[4][6],self.board[4][7],self.board[4][8]))
+		print("6 | {}  {}  {} | {}  {}  {} | {}  {}  {} | 6".format(self.board[5][0],self.board[5][1],self.board[5][2],self.board[5][3],self.board[5][4],self.board[5][5],self.board[5][6],self.board[5][7],self.board[5][8]))
+		print("  |-----------------------------|")
+		print("7 | {}  {}  {} | {}  {}  {} | {}  {}  {} | 7".format(self.board[6][0],self.board[6][1],self.board[6][2],self.board[6][3],self.board[6][4],self.board[6][5],self.board[6][6],self.board[6][7],self.board[6][8]))
+		print("8 | {}  {}  {} | {}  {}  {} | {}  {}  {} | 8".format(self.board[7][0],self.board[7][1],self.board[7][2],self.board[7][3],self.board[7][4],self.board[7][5],self.board[7][6],self.board[7][7],self.board[7][8]))
+		print("9 | {}  {}  {} | {}  {}  {} | {}  {}  {} | 9".format(self.board[8][0],self.board[8][1],self.board[8][2],self.board[8][3],self.board[8][4],self.board[8][5],self.board[8][6],self.board[8][7],self.board[8][8]))
+		print("   =============================")
+		print("    a  b  c   d  e  f   g  h  i")
+		print()
--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,42 +1,47 @@
 from SudokuBoard import *
 import random
 
+def shutDown():
+	global gameOver
+	gameOver = True
+
+def showHelp():
+	print()
+	print('=================================')
+	print('=      Available commands       =')
+	print('=================================')
+	print('<coords> <value> | e.g a1 1      ')
+	print('solve                            ')
+	print('help                             ')
+	print('exit                             ')
+	print('=================================')
+
+
+
 newBoard = SudokuBoard()
+
+commands = {"solve" : newBoard.solve, "help" : showHelp, "exit" : shutDown}
 
 gameOver = False
 while gameOver == False:
 
-	b = newBoard.returnValue()
+	newBoard.printBoard()
+	
+	userInput = input("??? ").split()
 
-	print()
-	print(" _____________________________")
-	print("| {}  {}  {} | {}  {}  {} | {}  {}  {} | 1".format(b[0][0],b[0][1],b[0][2],b[0][3],b[0][4],b[0][5],b[0][6],b[0][7],b[0][8]))
-	print("| {}  {}  {} | {}  {}  {} | {}  {}  {} | 2".format(b[1][0],b[1][1],b[1][2],b[1][3],b[1][4],b[1][5],b[1][6],b[1][7],b[1][8]))
-	print("| {}  {}  {} | {}  {}  {} | {}  {}  {} | 3".format(b[2][0],b[2][1],b[2][2],b[2][3],b[2][4],b[2][5],b[2][6],b[2][7],b[2][8]))
-	print("|-----------------------------|")
-	print("| {}  {}  {} | {}  {}  {} | {}  {}  {} | 4".format(b[3][0],b[3][1],b[3][2],b[3][3],b[3][4],b[3][5],b[3][6],b[3][7],b[3][8]))
-	print("| {}  {}  {} | {}  {}  {} | {}  {}  {} | 5".format(b[4][0],b[4][1],b[4][2],b[4][3],b[4][4],b[4][5],b[4][6],b[4][7],b[4][8]))
-	print("| {}  {}  {} | {}  {}  {} | {}  {}  {} | 6".format(b[5][0],b[5][1],b[5][2],b[5][3],b[5][4],b[5][5],b[5][6],b[5][7],b[5][8]))
-	print("|-----------------------------|")
-	print("| {}  {}  {} | {}  {}  {} | {}  {}  {} | 7".format(b[6][0],b[6][1],b[6][2],b[6][3],b[6][4],b[6][5],b[6][6],b[6][7],b[6][8]))
-	print("| {}  {}  {} | {}  {}  {} | {}  {}  {} | 8".format(b[7][0],b[7][1],b[7][2],b[7][3],b[7][4],b[7][5],b[7][6],b[7][7],b[7][8]))
-	print("| {}  {}  {} | {}  {}  {} | {}  {}  {} | 9".format(b[8][0],b[8][1],b[8][2],b[8][3],b[8][4],b[8][5],b[8][6],b[8][7],b[8][8]))
-	print(" =============================")
-	print("  a  b  c   d  e  f   g  h  i")
-	print()
+	if len(userInput) == 0:
+		pass
 
+	elif len(userInput) == 1:
+		commands[userInput[0]]()
 
+	elif len(userInput) == 2:
+		x, y = (ord(userInput[0][0]) - 97), (int(userInput[0][1]) - 1)
+		i = int(userInput[-1])
+		if newBoard.checkValidPlacement((x, y), i) == True:
+			newBoard.placeValue((x, y), i)
+		else:
+			print("\nInvalid placement.")
 
-	#userInput = input("??? ").split()
-	#x, y = (ord(userInput[0][0]) - 97), (int(userInput[0][1]) - 1)
-	#i = int(userInput[-1])
-	input("run?")
-	newBoard.solve()
-	#x = random.randint(0, 8)
-	#y = random.randint(0, 8)
-	#i = random.randint(1, 9)
-
-	#if newBoard.checkValidPlacement((x, y), i) == True:
-		#pass
-	#else:
-		#print("\nInvalid placement.")
+	else:
+		print("\nInvalid command.")
